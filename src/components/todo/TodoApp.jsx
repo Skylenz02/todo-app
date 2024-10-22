ff942c30b8bb7993822b305048709ae19e97f26a
banuprasad987@gmail.com <banuprasad987@gmail.com>
2024-10-23 01:47:22 +0530

    Step 7

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -5,18 +5,22 @@ import './TodoApp.css'
 export default function TodoApp() {
     return (
         <div className="TodoApp">
+            <HeaderComponent />            
+            
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={ <LoginComponent /> } />
                     <Route path='/login' element={ <LoginComponent /> } />
                     <Route path='/welcome/:username' element={<WelcomeComponent /> } />
                     <Route path='/todos' element={<ListTodosComponent /> } />
-
+                    <Route path='/logout' element={<LogoutComponent /> } />
+                    
                     <Route path='*' element={<ErrorComponent /> } />
 
                 </Routes>
             </BrowserRouter>
             
+            <FooterComponent />
             
         </div>
     )
@@ -147,6 +151,33 @@ function ListTodosComponent() {
                     </tbody>
 
                 </table>
+            </div>
+        </div>
+    )
+}
+
+function HeaderComponent() {
+    return (
+        <div className="header">
+            Header <hr/>
+        </div>
+    )
+}
+
+function FooterComponent() {
+    return (
+        <div className="footer">
+            <hr/> Footer 
+        </div>
+    )
+}
+
+function LogoutComponent() {
+    return (
+        <div className="LogoutComponent">
+            <h1>You are logged out!</h1>
+            <div>
+                Thank you for using our App. Come back soon!
             </div>
         </div>
     )
